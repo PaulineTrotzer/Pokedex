7041ef50703e400230768c0731215cb21723223d
Pauline Trotzer <pauline.trotzer@gmail.com>
2023-11-10 16:52:48 +0100

    basic style fit

--- js/script.js
+++ js/script.js
@@ -8,6 +8,25 @@ let backgroundColors = {
 
 }
 
+function init() {
+    includeHTML();
+    loadPokemon();
+}
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]')//Abfrage aller HTML-Elemente, Attribut: w3...//
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];//einzelnes Element des Array's//
+        file = element.getAttribute("w3-include-html");//Zeile liefert wert aus, also =header.html//
+        let resp = await fetch(file);//Laden von file, also vom header-Element//
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+
+}
 
 async function loadPokemon() {
     for (let i = 1; i < limitedPokemon; i++) {
@@ -18,7 +37,7 @@ async function loadPokemon() {
 
         allPokemons.push(currentPokemon);
         renderPokemonInfo(currentPokemon, i);
-    
+
     }
 }
 
@@ -30,21 +49,21 @@ function renderPokemonInfo(currentPokemon, i) {
     let category = currentPokemon['types'][0]['type']['name'];
 
     generatePokeCard(PokeId, name, image, category);
-
 }
 
 function generatePokeCard(PokeId, name, image, category) {
 
-document.getElementById('pokecard-main').innerHTML +=
+    document.getElementById('pokecard-main').innerHTML +=
      /*html*/`
      <div class="single-pokeCard">
-      <h5>${PokeId}</h5>
+
             <div class="name-category-img-container">
                 <div class="name-and-category-container">
                     <h1 id="pokemonName">${name}</h1>
                     <div id="category">${category}</div>
                 </div>
                 <img id="pokemonImage" src="${image}">
+                <h5>${PokeId}</h5>
             </div>
 </div>
     
